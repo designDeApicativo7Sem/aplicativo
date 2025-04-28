32ce80afeaa8c2a1121bd7737050b1310be187d2
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2025-04-28 02:06:41 +0000

    FEAT: integração das rotas da API com o front

--- SRC/scripts/login.js
+++ SRC/scripts/login.js
@@ -4,12 +4,13 @@ window.document.getElementById("submit").addEventListener("click", function() {
 });
 
 async function main(){
-    let url = "https://opulent-dollop-xjx4pj47v7ghv5xp-3000.app.github.dev"
+    let url = "https://opulent-dollop-xjx4pj47v7ghv5xp-3000.app.github.dev/"
     let credential = null;
     let params = null;
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+
     const errorMessage = document.getElementById("error-message");
 
     errorMessage.style.display = "none";
@@ -17,57 +18,46 @@ async function main(){
 
     if(email && password) {
         params = new URLSearchParams({
-            email: email,
-            senha: password
+            username: email,
+            password: password
         });
+
     } else {
-        errorMessage.style.display = "block";
+        errorMessage.style.display = "flex";
         errorMessage.textContent = "Por favor, preencha todos os campos.";
         return;
     }
 
     credential = await verifyCredentials(url, params);
 
-    if (credential) {
+    console.log(credential)
+
+    if (credential.message == "Login successful") {
 
         await localStorageData(credential);
         redirectToFeed()
 
-    } else {
-        errorMessage.style.display = "block";
-
-        if (credential.errorType === "email") {
-            errorMessage.textContent = "Email incorreto.";
-        } else if (credential.errorType === "password") {
-            errorMessage.textContent = "Senha incorreta.";
-        } else {
-            errorMessage.textContent = "Usuário inválido.";
-        }
+    }else{
+        errorMessage.style.display = "flex";
+        errorMessage.textContent = "Usuário inválido.";
     }
 }
 
 async function verifyCredentials(url, params) {
-    try {
-        const response = await fetch(`${url}/usuario?${params.toString()}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            },
-        });
 
-        if (response.status === 200) {
-            const data = await response.json(); 
-            return data[0];
-        } else {
-            console.error("Erro na requisição:", response.status);
-            return null;
-        }
-    } catch (error) {
-        console.error("Erro ao fazer a requisição:", error);
-        return null;
-    }
+    const response = await fetch(`${url}login?${params.toString()}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    });
+
+    const data = await response.json(); 
+    return data;
+
 }
+
 
 function redirectToFeed() {
     window.location.href = "../html/feed.html";
