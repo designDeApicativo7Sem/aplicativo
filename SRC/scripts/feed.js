63a003d3e23418c4044b38cc08c206fef3b42350
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2025-04-29 23:32:41 +0000

    feat: feature de adicionar post - background

---
+++ SRC/scripts/feed.js
@@ -0,0 +1,6 @@
+window.document.getElementById("addPost").addEventListener("click", () => {
+
+    // const elemento = document.getElementById("addPost"); // Altere para o id correto do seu elemento
+    // elemento.classList.toggle("oculto");
+    console.log("Classe alternada com sucesso!");
+});
