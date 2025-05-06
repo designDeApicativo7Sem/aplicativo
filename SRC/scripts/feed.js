4627a2f03b313460ac3c161eff98fc500e3db8bb
HIAN ARAUJO DAMACENO <hian.adamaceno@senacsp.edu.br>
2025-05-06 20:26:50 -0300

    feat: modal de post

--- SRC/scripts/feed.js
+++ SRC/scripts/feed.js
@@ -1,6 +1,13 @@
 window.document.getElementById("addPost").addEventListener("click", () => {
 
-    // const elemento = document.getElementById("addPost"); // Altere para o id correto do seu elemento
-    // elemento.classList.toggle("oculto");
-    console.log("Classe alternada com sucesso!");
+    const elemento = document.getElementById("background"); // Altere para o id correto do seu elemento
+    elemento.classList.toggle("oculto");
+
+    document.getElementById("cardPost").classList.toggle("oculto");
+    // console.log("Classe alternada com sucesso!");
 });
+
+window.document.getElementById("cancelar").addEventListener("click", () =>{
+    document.getElementById("background").classList.toggle("oculto");
+    document.getElementById("cardPost").classList.toggle("oculto");
+})
