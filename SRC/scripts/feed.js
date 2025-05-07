68b462de6618789fc79e746e4cf9789818b29a84
Hian A. Damaceno <132616428+55Hian@users.noreply.github.com>
2025-05-07 01:04:12 +0000

    .

--- SRC/scripts/feed.js
+++ SRC/scripts/feed.js
@@ -11,3 +11,36 @@ window.document.getElementById("cancelar").addEventListener("click", () =>{
     document.getElementById("background").classList.toggle("oculto");
     document.getElementById("cardPost").classList.toggle("oculto");
 })
+
+window.document.getElementById("registrar").addEventListener("click", () =>{
+    sendPost();
+})
+
+
+async function sendPost() {
+
+    var params = new URLSearchParams({
+        conteudo: document.getElementById("descricao").value,
+        foto: document.getElementById("foto").value,
+        tempo: document.getElementById("tempo").value
+        
+    });
+
+    var url = "https://apex.oracle.com/pls/apex/projeto_7/apigym/feed/"
+
+    const response = await fetch(`${url}usuario/?${params.toString()}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+    });
+    
+
+    const data = await response.json(); 
+
+    console.log(data);
+
+   
+    
+    }
